7d81d25ab54a97b0c2056ec34d76b81848cd2256
DNS <ivanov2161@gmail.com>
2022-05-26 19:40:16 +0500

    Have done four tasks

---
+++ LenghArray.py
@@ -0,0 +1,21 @@
+def lengthOfLongestSubstring(s):
+
+
+    def lengthOfLongestSubstring(s):
+        start = 0
+        maxlength = 0
+        substring = {}  # Определить подстроку, здесь {} - словарь, может хранить любой объект
+
+        for i, j in enumerate(s):
+            if j <= substring[j]: # если в подстроке
+                start = substring [j] + 1 # Окно сдвигается назад
+            else:
+                maxlength = max(maxlength, i - start + 1)
+                substring[j] = i
+
+        print(maxlength)
+        return maxlength
+
+
+
+print(lengthOfLongestSubstring('abcabcbb'))
