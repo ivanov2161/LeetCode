f214c105f13c8dd3538862edfb668cd19da360f9
DNS <ivanov2161@gmail.com>
2022-05-26 15:11:57 +0500

    Have done three tasks

--- LeetCode.py
+++ LeetCode.py
@@ -75,6 +75,57 @@ def palindrome_number(x):
         return False
 
 
+def add_two_numbers(l1, l2):  # Здесь я лоханулся и сделал с массивами задачу, а надо было с ListNode - займусь позже =)
+    """
+    Difficult - Medium
+    You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse
+    order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+    EXAMPLE: Input: l1 = [2,4,3], l2 = [5,6,4]
+                    Output: [7,0,8]
+                    Explanation: 342 + 465 = 807.
+    :param l1: Array
+    :param l2: Array
+    :return: Array of sum of reversed arrays
+    """
+
+    # Constraints
+
+    if len(l1) < 1 or len(l1) > 100 or len(l2) < 1 or len(l2) > 100:
+        print('Length of list should be in range(1, 100)')
+        return None
+
+    for i in l1:
+        if i < 0 or i > 9:
+            print('Numbers of list should be >= 0 and <= 9')
+            return None
+
+    for i in l2:
+        if i < 0 or i > 9:
+            print('Numbers of list should be >= 0 and <= 9')
+            return None
+
+    # Solution
+
+    answer = []
+
+    def reverse(array):
+        string = ''
+        for i in array[::-1]:
+            string += str(i)
+        return int(string)
+
+    numb_one = reverse(l1)
+    numb_two = reverse(l2)
+
+    for i in str(numb_one + numb_two):
+        answer.append(int(i))
+
+    return answer[::-1]  #
+
+
 two_sum([2, 7, 11, 15], 9)
 
-palindrome_number(121)
+print(palindrome_number(121))
+
+print(add_two_numbers([3, 4, 5], [2, 4, 5]))
